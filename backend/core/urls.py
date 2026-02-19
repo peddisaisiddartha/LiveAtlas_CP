1f1b4de79bf85cd6176ca590d32eea5ed6cb98ac
peddisaisiddartha <peddisaisiddartha1@gmail.com>
2026-02-19 14:31:09 +0530

    added root route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 from tours import views
 
+
+def home(request):
+    return HttpResponse("LiveAtlas Backend is Running 🚀")
+
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/tours/', views.get_tours),
     path('api/create-tour/', views.create_tour),
-    
-    # --- THIS LINE IS REQUIRED TO END CALLS ---
-    path('api/end-tour/<int:tour_id>/', views.end_tour), 
+    path('api/end-tour/<int:tour_id>/', views.end_tour),
 ]
 
 if settings.DEBUG:
