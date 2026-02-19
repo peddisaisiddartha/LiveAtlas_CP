6a2317d80494b9a518e7fb12b5fb7ebbd274d450
peddisaisiddartha <peddisaisiddartha1@gmail.com>
2026-02-19 11:52:20 +0530

    Remove venv from repo

---
+++ backend/Procfile
@@ -0,0 +1,1 @@
+web: daphne core.asgi:application --bind 0.0.0.0 --port $PORT
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dz@kpq^*o%hx)0kt=)a$4yv5q_w0(*z_7j+czg508tnfb+@of9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # ASCII configuration
 ASGI_APPLICATION = 'core.asgi.application'
