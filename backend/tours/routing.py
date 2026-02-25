1b9a61db788d6724410d96f44e18aff47ed1216b
peddisaisiddartha <peddisaisiddartha1@gmail.com>
2026-02-26 02:27:47 +0530

    again tried for fix of same error

--- backend/tours/routing.py
+++ backend/tours/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # This regex matches ws://localhost:8000/ws/tours/ROOM_ID/
-    re_path(r'ws/tours/(?P<room_id>\w+)/$', consumers.VideoConsumer.as_asgi()),
+    # This regex matches ws://localhost:8000/ws/tours/ROOM_NAME/
+    re_path(r'ws/tours/(?P<room_name>[-\w]+)/$', consumers.TourConsumer.as_asgi()),
 ]
