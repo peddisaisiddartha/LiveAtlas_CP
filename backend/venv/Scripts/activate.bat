595cef39861cda18d2f1f946b872a1e6a415999c
SaiSiddartha <peddisaisiddartha1@gmail.com>
2026-03-20 20:11:21 +0530

    sentry added successfully

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Dell\OneDrive\Desktop\liveatlas_cp\LiveAtlas_CP\backend\venv"
+set "VIRTUAL_ENV=D:\LiveAtlas_CP\backend\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
