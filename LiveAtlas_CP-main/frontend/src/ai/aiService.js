ed8a88dfa3affb3d89a4dbd4da4860271722fcda
Saisiddartha <liveatlasco@gmail.com>
2026-05-11 11:47:48 +0400

    ai issue tried

--- LiveAtlas_CP-main/frontend/src/ai/aiService.js
+++ LiveAtlas_CP-main/frontend/src/ai/aiService.js
@@ -5,7 +5,7 @@ export async function askAI(question) {
     const API_KEY = import.meta.env.VITE_GEMINI_KEY;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
