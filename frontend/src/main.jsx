7c47a28868cf93a4b49f43372b6bd8b2a442fbba
Saisiddartha <liveatlasco@gmail.com>
2026-05-18 14:03:04 +0400

    bloody uppercase c fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ErrorBoundary from "./components/ErrorBoundary";
+import ErrorBoundary from "./Components/ErrorBoundary";
 
 Sentry.init({
   dsn: "https://084baba49ed30f0e84f25b9bbda9217c@o4511077206130688.ingest.de.sentry.io/4511077219500112",
